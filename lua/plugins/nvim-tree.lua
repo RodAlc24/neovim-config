50c6042b365e143abfa15d832563e33583003f8c
Rodrigo Alcántara <rodrigo@rodalc.eu>
2025-12-06 21:04:20 +0100

    chore: change nvim tree icons

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -58,10 +58,10 @@ return {
 				enable = true,
 				show_on_dirs = true,
 				icons = {
-					hint = "",
-					info = "",
-					warning = "",
-					error = "",
+					hint = "󱐋",
+					info = "󰙎",
+					warning = "",
+					error = "",
 				},
 			},
 			update_focused_file = {
