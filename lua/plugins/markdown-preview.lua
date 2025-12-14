a856c63c701904cbcd736228a866c44cf1b5e754
Rodrigo Alcántara <rodrigo@rodalc.eu>
2025-12-14 22:47:17 +0100

    fix: markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,9 +2,9 @@ return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = "markdown",
-	build = function()
-		require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-		vim.fn["mkdp#util#install"]()
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
 	end,
 	keys = {
 		{
