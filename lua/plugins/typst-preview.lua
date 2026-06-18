91da9430426623a2b829ddd8be3a9a31494851c0
Rodrigo Alcántara <rodrigo@rodalc.eu>
2026-06-18 19:21:32 +0200

    feat: use typst preview in PDF with zathura
    Compiles the .typ file to a .pdf in /tmp, improving speed and
    avoiding a large amount of disk writes.

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -1,8 +1,21 @@
 return {
-	"chomosuke/typst-preview.nvim",
-	ft = "typst",
-	version = "1.*",
-	opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+	vim.lsp.config("tinymist", {
+		settings = {
+			exportPdf = "onType",
+			outputPath = "/tmp/$name",
+		},
+	}),
+
+	vim.api.nvim_create_user_command("TypstPreview", function()
+		local filepath = vim.api.nvim_buf_get_name(0)
+
+		if filepath:match("%.typ$") then
+			local basename = vim.fn.fnamemodify(filepath, ":t:r")
+			local pdf_path = "/tmp/" .. basename .. ".pdf"
+
+			vim.system({ "zathura", pdf_path })
+		end
+	end, {}),
 
 	vim.keymap.set("n", "<leader>TT", ":TypstPreview<cr>", { desc = "Typst Preview" }),
 }
