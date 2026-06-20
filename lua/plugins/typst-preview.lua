8ca40279c28dfa798871b20ca95db2661c036de2
Rodrigo Alcántara <rodrigo@rodalc.eu>
2026-06-20 23:11:38 +0200

    fix: typst preview with zathura now works
    It seems that tinymist wasn't able to create the file in /tmp if it
    didn't exist. Now it makes a touch when starting zathura so the file can
    be compiled properly.
    
    It is also necessary to modify the buffer (adding and deleting one blank
    line at the end of the file) so it forces a recompilation. The cursos
    position is preserved.

--- lua/plugins/typst-preview.lua
+++ lua/plugins/typst-preview.lua
@@ -13,6 +13,13 @@ return {
 			local basename = vim.fn.fnamemodify(filepath, ":t:r")
 			local pdf_path = "/tmp/" .. basename .. ".pdf"
 
+			vim.system({ "touch", pdf_path })
+
+			-- Force an update so the pdf is comiled
+			local line_count = vim.api.nvim_buf_line_count(0)
+			vim.api.nvim_buf_set_lines(0, line_count, line_count, false, { "" })
+			vim.api.nvim_buf_set_lines(0, line_count, line_count + 1, false, {})
+
 			vim.system({ "zathura", pdf_path })
 		end
 	end, {}),
